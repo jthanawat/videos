6bb5fa93c5ca3f486db984df40fc7abe8cdb40ae
jthanawat <kla13thanawat@gmail.com>
2021-02-10 23:33:38 +0700

    display a video player

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
 import youtube from '../apis/youtube';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
   state = {
     videos: [],
+    selectedVideo: null,
   };
+
+  componentDidMount() {
+    this.onTermSubmit('rov');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
@@ -14,14 +21,33 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
